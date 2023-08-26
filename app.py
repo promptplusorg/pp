50019c5d5067b7a94ba44dc55f1107921e6521b5
Your Name <you@example.com>
2023-08-26 21:27:42 +0700

    socker01

--- app.py
+++ app.py
@@ -339,3 +339,65 @@ async def websocket_endpoint(websocket: WebSocket):
         # This will log the full traceback of the exception
         logging.error(traceback.format_exc())
         await websocket.send_text(f"Error: {e}")
+
+
+@app.websocket("/chatline")
+async def websocket_endpoint(websocket: WebSocket):
+
+    await websocket.accept()
+    print("websocket.accept() accepted")
+
+    logging.info("WebSocket connection established.")
+    await websocket.send_text("WebSocket Connected")
+
+    try:
+
+        from embedchain import App
+        from dotenv import load_dotenv
+        load_dotenv()
+        ok = os.getenv('openai')
+        os.environ["OPENAI_API_KEY"] = ok
+        elon_musk_bot = App()
+
+        from .utilities import refresh_file
+        path_to_sandbox_folder, file_list_in_sandbox = refresh_file()
+
+        for i, file_name in enumerate(file_list_in_sandbox):
+            file_path = os.path.join(path_to_sandbox_folder, file_name)
+            try:
+                print(i+1, file_path)
+                elon_musk_bot.add(file_path, data_type='pdf_file')
+                print(i+1, "added")
+            except:
+                pass
+
+        while True:
+            data = await websocket.receive_text()
+            logging.info(f"Received WebSocket message: {data}")
+
+            prompt = "<h1>"+data+"</h1>"
+            print("prompt", prompt)
+            await websocket.send_text(prompt)
+
+            # do some lang chain here now
+            text = "How many companies does Elon Musk run?"
+            text = data
+            res = "error"
+            try:
+                print("text", text)
+                # res = chat(text)
+                # do you have embed files?
+                res = elon_musk_bot.query(text)
+                print("res", res)
+                ref_fmt = "<h1>"+res+"</h1>"
+                await websocket.send_text(ref_fmt)
+            except:
+                await websocket.send_text(res)
+
+    except WebSocketDisconnect as e:
+        logging.warning(f"WebSocket disconnected with code: {e.code}")
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        # This will log the full traceback of the exception
+        logging.error(traceback.format_exc())
+        await websocket.send_text(f"Error: {e}")
